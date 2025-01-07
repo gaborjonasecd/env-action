2161ecb8c1dd40db8bd20fb1c566a228eaae6d32
Gabor Jonas <gabor.jonas@ecigarettedirect.co.uk>
2025-01-07 08:45:49 +0000

    code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-/**
- * The entrypoint for the action.
- */
-const { run } = require('./main')
+const core = require('@actions/core');
+const fs = require('fs');
+
+async function run() {
+  const inputPrefix = "CI_";
+
+  try {
+    let envFileContent = '';
+
+    Object.keys(process.env).forEach(function(key) {
+      if(key.startsWith(inputPrefix)) {
+        envFileContent += `${key.substring(inputPrefix.length)}=${process.env[key]}\n`;
+      }
+    });
+
+    fs.writeFile('.env', envFileContent, function (error) {
+      if (error) {
+        core.setFailed(error.message);
+      }
+    });
+    fs.writeFile('.env.test', envFileContent, function (error) {
+      if (error) {
+        core.setFailed(error.message);
+      }
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
 
 run()
+
+module.exports = { run };
